04ceeb2b19ecbcb033af7d6ecb9eff25ac680484
RaytraceSG <azmi.maniku@gmail.com>
2024-11-16 19:53:47 +0800

    Add CI workflow, add vpc.tf, testing oidc

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "sctp-ce7-tfstate"                   # Terraform State bucket name
-    key    = "azmi1-simple-cicd-pipeline.tfstate" # Name of your tfstate file
+    key    = "ce7-grp-2-capstone.tfstate" # Name of your tfstate file
     region = "us-east-1"                          # Terraform State bucket region
   }
   required_version = "~> 1.9.5"
