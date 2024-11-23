bb99aa865cde7247c9d3f1eb473507723d2bce59
RaytraceSG <azmi.maniku@gmail.com>
2024-11-23 10:31:33 +0800

    add terraform-cd.yaml

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,4 +1,4 @@
 # Create Amazon ECR repository to store Docker images
 resource "aws_ecr_repository" "ecr_repo" {
-  name = "${var.name_prefix}-ecr"
+  name = "${var.name_prefix}-hello-world-ecr"
 }
