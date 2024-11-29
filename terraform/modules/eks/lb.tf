78875c29d90f7953f1c2d41fc6f966e6b2decb67
Alan Peh <alanpeh87@gmail.com>
2024-11-29 15:54:52 +0800

    Updating IAC - EKS with GHCR, Updated README.md

--- terraform/modules/eks/lb.tf
+++ terraform/modules/eks/lb.tf
@@ -1,54 +1,55 @@
 # Application Load Balancer (ALB)
-resource "aws_lb" "ce7_grp_2_lb" {
-  name               = var.lb_name
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = var.public_subnet_ids
+# resource "aws_lb" "ce7_grp_2_lb" {
+#   name               = var.lb_name
+#   internal           = false
+#   load_balancer_type = "application"
+#   security_groups    = [aws_security_group.lb_sg.id]
+#   subnets            = var.public_subnet_ids
 
-  # Enable access logging
-  access_logs {
-    bucket  = aws_s3_bucket.lb_logs.id
-    prefix  = "loadbalancer-logs"
-    enabled = true
-  }
-  tags = {
-    Name = var.lb_name
-  }
-}
-
-resource "aws_lb_listener" "ce7_grp_2_listener" {
-  load_balancer_arn = aws_lb.ce7_grp_2_lb.arn
-  port              = var.lb_listener_port
-  protocol          = var.lb_protocol
-
-  # Default action for the listener is to forward traffic to a target group
-  default_action {
-    # Type of action, in this case, forwarding traffic to a target group
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.ce7_grp_2_targrp.arn
-  }
-}
-
+#   # Enable access logging
+#   access_logs {
+#     bucket  = aws_s3_bucket.lb_logs.id
+#     prefix  = "loadbalancer-logs"
+#     enabled = true
+#   }
+#   tags = {
+#     Name = var.lb_name
+#   }
+# }
 # Define a target group to register targets (e.g., ECS instances)
-resource "aws_lb_target_group" "ce7_grp_2_targrp" {
-  name        = "${var.name_prefix}-targrp"
-  port        = var.lb_target_port
-  protocol    = var.lb_protocol
-  target_type = "ip"
-  vpc_id      = var.vpc_id
+# resource "aws_lb_target_group" "ce7_grp_2_targrp" {
+#   name = "${var.name_prefix}-targrp"
+#   port = 5000
+#   #port        = var.lb_target_port
+#   protocol    = var.lb_protocol
+#   target_type = "ip"
+#   vpc_id      = var.vpc_id
 
-  health_check {
-    interval = 30
+#   health_check {
+#     interval = 30
 
-    # Path for health check requests
-    path                = "/"
-    protocol            = var.lb_protocol
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-}
+#     # Path for health check requests
+#     path                = "/"
+#     port                = 5000
+#     protocol            = var.lb_protocol
+#     timeout             = 5
+#     healthy_threshold   = 2
+#     unhealthy_threshold = 2
+#   }
+# }
+
+# resource "aws_lb_listener" "ce7_grp_2_listener" {
+#   load_balancer_arn = aws_lb.ce7_grp_2_lb.arn
+#   port              = var.lb_listener_port
+#   protocol          = var.lb_protocol
+
+#   # Default action for the listener is to forward traffic to a target group
+#   default_action {
+#     # Type of action, in this case, forwarding traffic to a target group
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.ce7_grp_2_targrp.arn
+#   }
+# }
 
 # S3 bucket for ALB logs
 resource "aws_s3_bucket" "lb_logs" {
