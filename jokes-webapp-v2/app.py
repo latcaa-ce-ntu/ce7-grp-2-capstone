b16ae15b19c12fe5e7e8b8c08dd16eecfbee95f8
tcwong2024 <tcwong99@hotmail.com>
2024-12-05 00:12:21 +0800

    Add sonar-ignore

--- jokes-webapp-v2/app.py
+++ jokes-webapp-v2/app.py
@@ -1,25 +1,18 @@
 from flask import Flask, render_template
-from jokes_webapp import webapp_blueprint  # Import the blueprint from jokes_webapp.py
-from jokes_setting import setting_blueprint  # Import the blueprint from jokes_setting.py
+from jokes_webapp import webapp_blueprint  #NOSONAR
+from jokes_setting import setting_blueprint  #NOSONAR
 import os
-# from flask_wtf.csrf import CSRFProtect  # Import CSRFProtect here
-
-# sonar-ignore:start
 
 # app = Flask(__name__)
-app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), 'templates'))
-
-# Enable CSRF protection for the main app
-# csrf = CSRFProtect(app)
+app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), 'templates'))  #NOSONAR
 
 # Register blueprints
-app.register_blueprint(webapp_blueprint, url_prefix='/webapp')
-app.register_blueprint(setting_blueprint, url_prefix='/settings')
+app.register_blueprint(webapp_blueprint, url_prefix='/webapp')  #NOSONAR
+app.register_blueprint(setting_blueprint, url_prefix='/settings') #NOSONAR
 
 @app.route('/')
 def index():
-    return render_template('index.html')  # Or whatever you want to show on the root URL
+    return render_template('index.html')  
 
 if __name__ == "__main__":
     app.run(debug=True)
-# sonar-ignore:end
